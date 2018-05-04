66ca73bc84844232413be7c4fe5d8c6607fcbb1e
silvestrevivo <silvestrevivomillan@gmail.com>
2018-05-04 09:37:54 +0200

    adding body-parser and nodemon

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const express = require('express');
+const express = require('express'),
+  bodyParser = require('body-parser');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log('RESTapi running on http://localhost:3000');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log(`RESTapi running on http://localhost:${port}`);
 });
