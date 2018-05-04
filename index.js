4744102b5411f5d6305c9e5f30ffa9ab21f532a3
silvestrevivo <silvestrevivomillan@gmail.com>
2018-05-04 13:57:11 +0200

    mongoose added to listen to the data base local

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express'),
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose');
 
 // basic configuration to run server with express
 const app = express();
@@ -15,7 +16,19 @@ app.get('/hello/:message', (req, res) => {
   //http://localhost:3000/hello/world => {"message":"Hello world!!"}
 });
 
-// making the app listening to port
-app.listen(port, () => {
-  console.log(`RESTapi running on http://localhost:${port}`);
-});
+// First connect to the data base and server runing as callback
+// to ensure that both are working at the same time
+// localhost:27017 => by default the url to access to the data bases
+// /param => name of the database we have created via the terminal
+mongoose.connect('mongodb://localhost:27017/shop', (err, res) => {
+  if (err) {
+    return console.log(`Error connecting to the data base: ${err}`)
+  }
+  console.log('Connected to the data base...')
+
+  // making the app listening to port
+  app.listen(port, () => {
+    console.log(`RESTapi running on http://localhost:${port}`);
+  });
+})
+
