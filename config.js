925a963c3d118b37af9e2feab34a04cafcabb688
silvestrevivo <silvestrevivomillan@gmail.com>
2018-05-07 21:12:50 +0200

    refractoring comments

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
   db: process.env.MONGODB || 'mongodb://localhost:27017/shop'
+  //localhost:27017 => by default the url to access to the data bases
 }
--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose');
 const app = require('./app');
 
-// basic configuration to run server with express
+// file configuration to run server with express
 const config = require('./config');
 
 // First connect to the data base and server runing as callback
 // to ensure that both are working at the same time
-// localhost:27017 => by default the url to access to the data bases
-// /param => name of the database we have created via the terminal
 mongoose.connect(config.db, (err, res) => {
   if (err) {
     return console.log(`Error connecting to the data base: ${err}`)
@@ -16,7 +14,7 @@ mongoose.connect(config.db, (err, res) => {
 
   // making the app listening to port
   app.listen(config.port, () => {
-    console.log(`RESTapi running on http://localhost:${config.port}`);
+    console.log(`RESTapi running on http://localhost:${port}`);
   });
 })
 
